8598f1cc9a964515ee44b9c93df6464d35b8909d
Ricardo <ricoazeredo@yahoo.com.br>
2022-10-24 15:04:44 -0300

    add logout, bloqueio, sessão e mensagens

--- aula-7/Login/index.php
+++ aula-7/Login/index.php
@@ -1,8 +1,10 @@
 <?php
+    session_start();
+    ob_start(); //limpa o buffer de saída. Usado no redirecionamento.
     require 'head.php';
     require 'config.php';
     $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-    var_dump($dados);
+    //var_dump($dados);
     if(!empty($dados['SendLogin'])){
     
         $sql = $pdo->prepare("SELECT * FROM tbl_login WHERE email = :email");
@@ -11,14 +13,23 @@
 
         if(($sql) && ($sql->rowCount() != 0)){
             $resultado = $sql->fetch(PDO::FETCH_ASSOC);
-            var_dump($resultado);
+            //var_dump($resultado);
             if(password_verify($dados['password'], $resultado['senha'])){
-                
+                $_SESSION['id'] = $resultado['id'];
+                $_SESSION['nome'] = $resultado['nome'];
                 header('Location: home.php');
                 exit;
             } else {
-                echo "Erro no Login";
+                $_SESSION['msg'] = "<p style='color: #ff0000'>Erro: Usuário ou senha inválida!</p>";
             }
+        } else {
+            $_SESSION['msg'] = "<p style='color: #ff0000'>Erro: Usuário ou senha inválida!</p>";
+        }
+
+        if(isset($_SESSION['msg'])){
+            echo $_SESSION['msg'];
+            //Destroi a mensagem para não imprimir na tela novamente
+            unset($_SESSION['msg']);
         }
     }
 ?>
@@ -30,7 +41,12 @@
         <div class="mb-3">
             <label for="" class="form-label">
                 Email: <br/>
-                <input type="email" name="email" class="form-control">
+                <input
+                 type="email" 
+                 name="email" 
+                 class="form-control" 
+                 value="<?php if(isset($dados['email'])){ echo $dados['email']; } ?>"
+                >
             </label>
         </div>
         <div class="mb-3">

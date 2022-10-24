b68961672e674204002c18c113de4a001dbe402c
Ricardo <ricoazeredo@yahoo.com.br>
2022-10-23 22:48:17 -0300

    Criando o processo de update - edição

--- aula-7/cad_usuario/index.php
+++ aula-7/cad_usuario/index.php
@@ -1,3 +1,23 @@
+<?php
+    require 'config.php';
+
+    /**
+     * Cria uma lista vazia para receber os dados da consulta da tabela tbl_usuarios.
+     * Depois, verifica se tem dados na cosnulta(se quantidade de linhas é maior que 0 (zero)).
+     * Caso tenha, add no array lista.
+     * fecthAll - pega todos os dados. 
+     * PDO::FECTH_ASSOC - Cosntante estática do PDO que associa os dados. Tira duplicidades do array.
+     */
+
+    $lista = [];
+    $sql = $pdo->query("SELECT * FROM tbl_usuarios");
+
+    if($sql->rowCount() > 0) {
+        $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+        //print_r($lista);
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,7 +38,17 @@
                 <th>Email</th>
                 <th>Ações</th>
             </tr>
-            
+            <?php foreach($lista as $usuario): ?>
+                <tr>
+                    <td><?php echo $usuario['id']; ?></td>
+                    <td><?=$usuario['nome']; ?></td>
+                    <td><?=$usuario['email']; ?></td>
+                    <td>
+                        <a href="editar.php?id=<?=$usuario['id']; ?>">editar</a>
+                        <a href="excluir.php?id=<?=$usuario['id']; ?>">excluir</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
         </table>
     </div>
 </body>

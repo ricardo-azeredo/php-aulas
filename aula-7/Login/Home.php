1ce62c73148664dcdf77304c21bded1925c6d094
Ricardo <ricoazeredo@yahoo.com.br>
2022-10-24 14:32:46 -0300

    criando o login

---
+++ aula-7/Login/Home.php
@@ -0,0 +1,7 @@
+<?php
+    require 'head.php';
+?>
+<h1>Home</h1>
+
+
+<?php include 'footer.php'; ?>
--- aula-7/Login/index.php
+++ aula-7/Login/index.php
@@ -1,11 +1,32 @@
 <?php
     require 'head.php';
+    require 'config.php';
+    $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+    var_dump($dados);
+    if(!empty($dados['SendLogin'])){
+    
+        $sql = $pdo->prepare("SELECT * FROM tbl_login WHERE email = :email");
+        $sql->bindParam(':email', $dados['email'], PDO::PARAM_STR);
+        $sql->execute();
+
+        if(($sql) && ($sql->rowCount() != 0)){
+            $resultado = $sql->fetch(PDO::FETCH_ASSOC);
+            var_dump($resultado);
+            if(password_verify($dados['password'], $resultado['senha'])){
+                
+                header('Location: home.php');
+                exit;
+            } else {
+                echo "Erro no Login";
+            }
+        }
+    }
 ?>
 
 <div class="container">
     <h1>Login</h1>
 
-    <form action="verifica_login.php" method="post">
+    <form action="" method="post">
         <div class="mb-3">
             <label for="" class="form-label">
                 Email: <br/>
@@ -18,6 +39,8 @@
                 <input type="password" name="password" class="form-control">
             </label>
         </div>
+
+        <input type="submit" value="Salvar" name="SendLogin" class="btn btn-primary" />
     </form>
     <h5><a type="button" class="btn btn-link" href="inscrever.php">Inscrever</a></h5>
 
